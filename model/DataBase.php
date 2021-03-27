51a4df0308531ad21d60f458bd63140148def8d9
democvidev <cnvvc@mail.ru>
2021-03-27 21:43:01 +0100

    styled the timer-element

--- model/DataBase.php
+++ model/DataBase.php
@@ -19,16 +19,16 @@ class DataBase {
     public function dbConnect() {
 
         try {
-            $this->pdo = new \PDO
-            ('mysql:host=' . $this->host . ';dbname=' . $this->database, $this->username, $this->password, [
-                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+            $this->pdo = new PDO(
+                'mysql:host=' . $this->host . ';dbname=' . $this->database, $this->username, $this->password, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
                 ]);
                 $this->pdo->exec("SET NAMES UTF8");
                 
                 return $this->pdo;
             }
-            catch (\Exception $e) {
+            catch (PDOException $e) {
                 die('Erreur : ' . $e->getMessage()) or die(print_r($this->pdo->errorInfo()));
             }
     }
