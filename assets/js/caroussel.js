c7806002472fa2a6d1f48dd4815cc45d4e9b6ef6
Vladislav <cnvvc_vlad@mail.ru>
2021-11-14 14:59:37 +0100

    add DOMContentLoaded & updated file

--- assets/js/caroussel.js
+++ assets/js/caroussel.js
@@ -1,108 +1,117 @@
-var slideIndex, slides, dots, captionText, captionTitle;
+window.addEventListener('DOMContentLoaded', () => {
+  var slideIndex, slides, dots, captionText, captionTitle
 
-initGallery();
+  initGallery()
 
-function initGallery() {
+  function initGallery() {
+    slideIndex = 0
+    slides = document.getElementsByClassName('imageHolder')
+    if (slides[slideIndex]) {
+      slides[slideIndex].style.opacity = 1
 
-    slideIndex = 0;
-    slides = document.getElementsByClassName("imageHolder");
-    slides[slideIndex].style.opacity = 1;
+      captionText = document.querySelector('.captionHolder .captionText')
+      captionTitle = document.querySelector('.captionHolder .captionTitle')
 
-    captionText = document.querySelector(".captionHolder .captionText");
-    captionTitle = document.querySelector(".captionHolder .captionTitle");
+      captionText.innerText = slides[slideIndex].querySelector(
+        '.captionText',
+      ).innerText
+      captionTitle.innerText = slides[slideIndex].querySelector(
+        '.captionTitle',
+      ).innerText
+    }
 
-
-    captionText.innerText = slides[slideIndex].querySelector(".captionText").innerText;
-    captionTitle.innerText = slides[slideIndex].querySelector(".captionTitle").innerText;
-
-
-    dots = [];
-    var dotsContainer = document.getElementById("dotsContainer");
+    dots = []
+    var dotsContainer = document.getElementById('dotsContainer')
 
     for (var i = 0; i < slides.length; i++) {
-        var dot = document.createElement("span");
-        dot.classList.add("dots");
-        dot.setAttribute("onClick", "moveSlide(" + i + ")");
-        dotsContainer.append(dot);
-        dots.push(dot);
+      var dot = document.createElement('span')
+      dot.classList.add('dots')
+      dot.setAttribute('onClick', 'moveSlide(' + i + ')')
+      dotsContainer.append(dot)
+      dots.push(dot)
     }
+    if (dots[slideIndex]) {
+      dots[slideIndex].classList.add('active')
+    }
+  }
 
-    dots[slideIndex].classList.add("active");
+  function plusSlides(n) {
+    moveSlide(slideIndex + n)
+  }
 
-}
-
-function plusSlides(n) {
-    moveSlide(slideIndex + n);
-}
-
-function moveSlide(n) {
-    var i, current, next;
+  function moveSlide(n) {
+    var i, current, next
     var moveSlideAnimClass = {
-        forCurrent: "",
-        forNext: ""
+      forCurrent: '',
+      forNext: '',
     }
-    var slideTextAnimClass;
-    var slideTitleAnimClass;
+    var slideTextAnimClass
+    var slideTitleAnimClass
     if (n > slideIndex) {
-        if (n >= slides.length) { n = 0 }
-        moveSlideAnimClass.forCurrent = "moveLeftCurrentSlide";
-        moveSlideAnimClass.forNext = "moveLeftNextSlide";
-        slideTextAnimClass = "slideTextFromTop";
-        slideTitleAnimClass = "slideTitleFromTop";
-
+      if (n >= slides.length) {
+        n = 0
+      }
+      moveSlideAnimClass.forCurrent = 'moveLeftCurrentSlide'
+      moveSlideAnimClass.forNext = 'moveLeftNextSlide'
+      slideTextAnimClass = 'slideTextFromTop'
+      slideTitleAnimClass = 'slideTitleFromTop'
     } else if (n < slideIndex) {
-        if (n < 0) { n = slides.length - 1 }
-        moveSlideAnimClass.forCurrent = "moveRightCurrentSlide";
-        moveSlideAnimClass.forNext = "moveRightNextSlide";
-        slideTextAnimClass = "slideTextFromBottom";
-        slideTitleAnimClass = "slideTitleFromBottom";
-
+      if (n < 0) {
+        n = slides.length - 1
+      }
+      moveSlideAnimClass.forCurrent = 'moveRightCurrentSlide'
+      moveSlideAnimClass.forNext = 'moveRightNextSlide'
+      slideTextAnimClass = 'slideTextFromBottom'
+      slideTitleAnimClass = 'slideTitleFromBottom'
     }
 
     if (n != slideIndex) {
-        next = slides[n];
-        current = slides[slideIndex];
-        for (i = 0; i < slides.length; i++) {
-            slides[i].className = "imageHolder";
-            slides[i].style.opacity = 0;
-            dots[i].classList.remove("active");
-        }
-        current.classList.add(moveSlideAnimClass.forCurrent);
-        next.classList.add(moveSlideAnimClass.forNext);
-        dots[n].classList.add("active");
-        slideIndex = n;
-
+      next = slides[n]
+      current = slides[slideIndex]
+      for (i = 0; i < slides.length; i++) {
+        slides[i].className = 'imageHolder'
+        slides[i].style.opacity = 0
+        dots[i].classList.remove('active')
+      }
+      current.classList.add(moveSlideAnimClass.forCurrent)
+      next.classList.add(moveSlideAnimClass.forNext)
+      dots[n].classList.add('active')
+      slideIndex = n
     }
-    captionText.style.display = "none";
-    captionText.className = "captionText" + slideTextAnimClass;
-    captionText.innerText = slides[n].querySelector(".captionText").innerText;
-    captionText.style.display = "block";
+    if (captionText) {
+      captionText.style.display = 'none'
+      captionText.className = 'captionText' + slideTextAnimClass
+      captionText.innerText = slides[n].querySelector('.captionText').innerText
+      captionText.style.display = 'block'
 
-    captionTitle.style.display = "none";
-    captionTitle.className = "captionTitle" + slideTitleAnimClass;
-    captionTitle.innerText = slides[n].querySelector(".captionTitle").innerText;
-    captionTitle.style.display = "block";
-}
+      captionTitle.style.display = 'none'
+      captionTitle.className = 'captionTitle' + slideTitleAnimClass
+      captionTitle.innerText = slides[n].querySelector(
+        '.captionTitle',
+      ).innerText
+      captionTitle.style.display = 'block'
+    }
+  }
 
-var timer = null;
-function setTimer() {
+  var timer = null
+  function setTimer() {
     timer = setInterval(function () {
-        plusSlides(1);
+      plusSlides(1)
     }, 5000)
+  }
+  setTimer()
 
-}
-setTimer();
-
-function playPauseSlides() {
-    var playPauseSlides = document.getElementById("playPauseBtn");
+  function playPauseSlides() {
+    var playPauseSlides = document.getElementById('playPauseBtn')
     if (timer == null) {
-        setTimer();
-        playPauseBtn.style.backgroundPositionY = "0px";
-        playPauseBtn.title = "Pause";
+      setTimer()
+      playPauseBtn.style.backgroundPositionY = '0px'
+      playPauseBtn.title = 'Pause'
     } else {
-        clearInterval(timer);
-        timer = null;
-        playPauseBtn.style.backgroundPositionY = "-40px";
-        playPauseBtn.title = "Play";
+      clearInterval(timer)
+      timer = null
+      playPauseBtn.style.backgroundPositionY = '-40px'
+      playPauseBtn.title = 'Play'
     }
-}
+  }
+})
