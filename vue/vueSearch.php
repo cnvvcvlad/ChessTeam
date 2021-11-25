e29e766d69fbc7a1aaf74eb46c480aa345707a1d
Vladislav <cnvvc_vlad@mail.ru>
2021-11-25 22:42:26 +0100

    add search option & styled pages

---
+++ vue/vueSearch.php
@@ -0,0 +1,30 @@
+<?php ob_start(); ?>
+<?php $title = 'Résultats de la recherche'; ?>
+<?php $description = 'Page de résultats de la recherche'; ?>
+<div class="container">
+    <h1>Resultats de la recherche :</h1>
+    <div>
+        <?php if(isset($searchResults) && $searchResults != null): ?>
+        <ul>
+            <?php foreach ($searchResults as $key => $value) : ?>
+                <li>
+                    <a href="?action=allArticles&amp;id=<?= $value->getId() ?>">
+                        <?= $value->getArt_title() ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+        <?php else : ?>
+            <p>Aucun résultat</p>
+        <?php endif; ?>
+    </div>
+</div>
+<div class="back-page">
+    <a href="<?= substr(
+        basename($_SERVER['HTTP_REFERER']),
+        0
+    ) ?>">Retour</a>
+    <a href="?action=home">Retour à l'accueil</a>
+</div>
+<?php $template = ob_get_clean(); ?>
+<?php require 'templates/tempAccueil.php'; ?>
