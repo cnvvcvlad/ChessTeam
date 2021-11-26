ffa366cd3725e7b4d201a58a9e1b337b716c8f9a
Vladislav <cnvvc_vlad@mail.ru>
2021-11-26 22:24:46 +0100

    reorganized files

---
+++ vue/shared/_aside_left.php
@@ -0,0 +1,39 @@
+<aside class="form_view">
+    <div class="top_article_form">
+        <?php if (!empty($lastArticle_one)): ?>
+            <?php foreach ($lastArticle_one as $key => $value): ?>
+                <h2><img src="assets/img/by_default/ico_epingle.png" alt="Catégorie" class="ico_categorie" /><?= $value->getArt_title() ?></h2>
+                <?php if (isAdmin()): ?>
+                    <div class="bouton_commande"><a href="">Modifier</a></div>
+                <?php endif; ?>
+                <div class="bouton_commande">
+                    <a href="?action=allArticles&amp;id=<?= $value->getId() ?>" class="bouton_rouge">Voir
+                        l'article <img src="assets/img/by_default/flecheblanchedroite.png" alt="le bouton rouge" /></a>
+                </div>
+                <p>Ecrit par
+                    <span class="mark"><?= showNameAuthor(
+                        $value->getArt_author()
+                    ) ?></span>
+                    le <em><?= $value->getArt_date_creation() ?></em> dans la catégorie
+                    <strong><?= showNameCategory(
+                        $value->getCategory_id()
+                    ) ?></strong>
+                </p>
+                <div id="detail_art" class="justify_form">
+                    <img src="assets/img/uploads/<?= $value->getArt_image() ?>" alt="Image de l'article">
+                    <span><a href="#cache"> [Lire la suite...] </a></span>
+                    <div id="cache">
+                        <span><?= $value->getArt_description() ?><br>
+                            <a href="#detail_art">[Voir moins]</a>
+                        </span>
+                    </div>
+                    <p><a href="?action=allArticles&amp;id=<?= $value->getId() ?>">Commentaires
+                            (<?= numberCommentsOfArticle(
+                                getAllCommentsOfArticle($value->getId())
+                            ) ?>)</a>
+                    </p>
+                </div>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </div>
+</aside>
---
+++ vue/shared/_body_form.php
@@ -0,0 +1,41 @@
+<body>
+    <?php include_once 'vue/shared/_header.php'; ?>
+    <div id="main_wrapper">
+
+        <?php if (!isAdmin()) : ?>
+        <div class="form_view">
+                <?php include_once 'vue/shared/_aside_left.php'; ?>                
+                <main class="container_form">
+                    <?php
+                    if (isset($_GET['alert']) and $_GET['alert'] == 'errorContact') {
+                        echo '<h4>Veuillez rééssayer. Une erreur c\'est produite.</h4>';
+                    }
+                    ?>
+
+                    <?= $template_form; ?>
+                </main>         
+
+                <?php include_once 'vue/shared/_aside_right.php'; ?>
+
+            <?php else : ?>
+                <main class="container_form">
+
+                    <?= $template_form; ?>
+
+                </main>
+            </div>
+            <?php endif; ?>
+        </div>
+
+        
+        <?php include_once 'vue/shared/_footer.php'; ?>
+    <!-- ==javaScript perso== -->
+    <script src="assets/js/caroussel.js"></script>
+    <script src="assets/js/burger.js"></script>
+    <script src="assets/js/script.js"></script>
+
+    <!--reCAPTCHA-->
+    <script src="https://www.google.com/recaptcha/api.js?render=6LcgZ-oUAAAAAKdW6gHFYFBm7Qx-d52XntvALZma"></script>
+</body>
+
+</html>
