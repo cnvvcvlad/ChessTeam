30ba3af4cd001276783723c3e9ef8b4ec8d38005
Vladislav <cnvvc_vlad@mail.ru>
2021-11-26 20:30:26 +0100

    updated  recaptcha location

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {    
+// Recaptcha
+if (document.getElementById('recaptchaResponse')) {
+    grecaptcha.ready(function () {
+      grecaptcha
+        .execute('6LcgZ-oUAAAAAKdW6gHFYFBm7Qx-d52XntvALZma', {
+          action: 'homepage',
+        })
+        .then(function (token) {
+          document.getElementById('recaptchaResponse').value = token
+        })
+    })
+  }
+});
