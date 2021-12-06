1d0d278c6aed80e1fb2455a723c88e941818f9f2
Vladislav <cnvvc_vlad@mail.ru>
2021-12-06 22:53:15 +0100

    refactoring file

--- src/controller/HomeController.php
+++ src/controller/HomeController.php
@@ -8,14 +8,19 @@ use Democvidev\ChessTeam\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     private $articleManager;
+    private $offset = 40;
+    private $limit = 5;
+    private $id_post = 77;
+
     public function __construct()
     {
-        $this->articleManager = new ArticleManager();
+        $this->articleManager = new ArticleManager($this->getDatabase());
     }
+
     public function index()
     {
-        $lastArticles = $this->articleManager->affichageRecentes();
-        $lastArticle_one = $this->articleManager->affichageLastOne();        
+        $lastArticles = $this->articleManager->affichageRecentes($this->offset, $this->limit);
+        $lastArticle_one = $this->articleManager->affichageOne($this->id_post);        
         return $this->view('home.index', [
             'lastArticles' => $lastArticles,
             'lastArticle_one' => $lastArticle_one
