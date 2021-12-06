90a99128b47f314c1a1721a120008473260cdf78
Vladislav <cnvvc_vlad@mail.ru>
2021-12-06 22:51:17 +0100

    updated file and add getDB function

--- src/controller/AbstractController.php
+++ src/controller/AbstractController.php
@@ -2,8 +2,18 @@
 
 namespace Democvidev\ChessTeam\Controller;
 
-class AbstractController
-{
+use Democvidev\ChessTeam\Database\DataBaseConnection;
+
+abstract class AbstractController
+{   
+
+    /**
+     * Transmit data to the view
+     *
+     * @param string $path
+     * @param array|null $params
+     * @return void
+     */
     public function view(string $path, array $params =null)
     {
         // bufferisation de la vue
@@ -15,6 +25,15 @@ class AbstractController
         }
         $content = ob_get_clean();
         require VIEWS . 'baseLayout.php';
+    }
 
+    /**
+     * Retourne une instance de la classe DataBaseConnection
+     *
+     * @return DataBaseConnection
+     */
+    public function getDatabase(): DataBaseConnection
+    {
+        return new DataBaseConnection();
     }
 }
