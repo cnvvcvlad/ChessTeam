ee1fe21b28ecf2032434b5caeba97060d1537053
Vladislav <cnvvc_vlad@mail.ru>
2021-12-06 23:00:11 +0100

    add new views

---
+++ vue/categories/show.php
@@ -0,0 +1,77 @@
+<?php $title = 'Catégorie des articles'; ?>
+<?php $description =
+    'Découvrez les catégories disponible pour trouver les informations utiles'; ?>
+
+    <div class="top_article">
+        <?php if (!empty($params['category'])): ?>
+            <?php foreach ($params['category'] as $key => $value): ?>
+                <h1>Catégorie : <?= $value->getTitle() ?> </h1>
+                <?php if (isset($this->role) && $this->role->isAdmin()): ?>
+                    <div class="banniere_bouton">
+                        <div class="bouton_commande" ><a href="?action=allCategory&amp;deleteC=<?= $value->getId() ?>" onclick="return(confirm('Etes-vous sûr de vouloir supprimer?'));">Supprimer cette catégorie</a>
+                        </div>
+                    </div>
+                <?php endif; ?>
+                <p><span class="information">Ecrit par</span>
+                    <span class="mark"><?= isset($user) ? $user->showNameAuthor(
+                        $value->getCat_author()
+                    ) : '' ?></span>
+                    le <em><?= $value->getCat_date_creation() ?></em></p>
+
+                <div class="justify_article">
+                    <a class="grand_image" href="<?= SCRIPTS . 'img' . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $value->getCategory_image() ?>">
+
+                    <img src="<?= SCRIPTS . 'img' . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $value->getCategory_image() ?>" alt="Image de la catégorie" title="Cliquez pour agrandir"></a>
+                    <span><h3><?= $value->getDescription() ?></h3></span>
+                </div>
+
+
+            <?php endforeach; ?>
+
+
+        <?php endif; ?>
+    </div>
+
+<?php if (isset($this->role) && $this->role->isAdmin()): ?>
+    <h3>Modifier cette catégorie</h3>
+    <form action="?action=categoryForm" method="post" class="form-create"
+          enctype="multipart/form-data">
+        <fieldset>
+            <legend>Introduisez vos informations</legend>
+            <div class="form-create">
+                <label for="">Titre de la catégorie
+                    <input type="hidden" name="id" value="<?= $value->getId() ?>">
+
+                    <p><input type="text" required name="title" value="<?= $value->getTitle() ?>"></p>
+                </label>
+            </div>
+            <div class="form-create">
+                <label for="">Description de la catégorie
+                    <p><textarea required name="description" placeholder="Description de la catégorie" rows="5"
+                                 cols="33"><?= $value->getDescription() ?></textarea></p>
+                </label>
+            </div>
+            <div class="form-create">
+                <label class="form-create" for="image_cat">Choisir une image</label>
+                    <p><input type="file" name="category_image" class="image" id="image_cat" accept='.gif, .png , .jpg' required></p>
+
+            </div>
+            <p><input type="hidden" name="cat_author"  value="<?= htmlspecialchars(
+                $_SESSION['id_user']
+            ) ?>"></p>
+
+            <div class="form-create">
+                <p><input type="submit" value="Envoyer" name="updateCategory"></p>
+                <p><input type="reset" value="Annuler" /></p>
+
+            </div>
+        </fieldset>
+    </form>
+<?php endif; ?>
+        <div class="back-page">
+            <a href="<?= dirname(SCRIPTS) ?>">Retour à la page d'accueil</a>
+            <a href="<?= dirname(SCRIPTS) . DIRECTORY_SEPARATOR . 'category' . DIRECTORY_SEPARATOR . $value->getId() . DIRECTORY_SEPARATOR . 'posts' ?>" class="bouton_rouge">Consulter
+                les articles de cette catégorie 
+                <img src="<?= SCRIPTS . 'img' . DIRECTORY_SEPARATOR . 'by_default' . DIRECTORY_SEPARATOR . 'flecheblanchedroite.png' ?>" alt="le bouton rouge"/>
+            </a>
+        </div>
---
+++ vue/posts/category.php
@@ -0,0 +1,54 @@
+<?php $title = 'Articles d\'une seule catégorie'; ?>
+<?php $description = 'Les textes sont triés par différents sous-domaine du monde des échecs'; ?>
+
+
+    <div class="top_article">
+        <?php if (!empty($params['posts'])): ?>
+            <?php foreach ($params['posts'] as $key => $value) : ?>
+
+                <h1><?= $value->getArt_title() ?></h1>
+                <p><span class="information"> Ecrit par</span>
+                    <span class="mark"><?= isset($user) ? $user->showNameAuthor($value->getArt_author()) : '' ?></span>
+                    le <em><?= $value->getArt_date_creation() ?></em> <span class="information"> dans la catégorie</span>
+                    <strong><?= isset($category) ? $category->showNameCategory($value->getCategory_id()) : '' ?></strong></p>
+
+                <div class="banniere_bouton">
+                    <div class="bouton_commande">
+                        <a href="<?= dirname(SCRIPTS) ?>/posts/<?= $value->getId() ?>" class="bouton_rouge">Voir l'article
+                            <img src="<?= SCRIPTS . 'img' . DIRECTORY_SEPARATOR . 'by_default' . DIRECTORY_SEPARATOR . 'flecheblanchedroite.png' ?>" alt="le bouton rouge"/></a>
+
+                    </div>
+                    <?php if (isset($this->role) && $this->role->isAdmin()) : ?>
+                        <div class="bouton_commande"><a href="?action=allArticles&amp;updateA=<?= $value->getId() ?>">Modifier</a></div>
+                    <?php endif; ?>
+                </div>
+
+                <div class="justify_article">
+                    <p>
+                        <a class="grand_image" href="<?= SCRIPTS . 'img' . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $value->getArt_image() ?>">
+                    <img src="<?= SCRIPTS . 'img' . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $value->getArt_image() ?>" alt="Image de la catégorie" title="Cliquez pour agrandir">
+                </a>
+
+            <span>
+                <?= $value->getArt_description() ?></span>
+                    </p>
+
+                    <p>
+                        <a href="<?= dirname(SCRIPTS) ?>/posts/<?= $value->getId() ?>">(<?= isset($params['comment']) ? count($params['comment']->getAllCommentsOfArticle($value->getId())) : '' ?>
+                            ) Commentaires </a></p>
+
+                </div>
+            <div class="separateur"></div>
+
+
+
+            <?php endforeach; ?>
+        <?php else : ?>
+            <h3>Il n'y a pas d'articles dans cette catégorie</h3>
+
+        <?php endif; ?>
+    </div>
+    <div class="back-page">
+        <a href="<?= substr(basename($_SERVER['HTTP_REFERER']), 0) ?>">Retour</a>
+        <a href="?action=home">Retour à l'accueil</a>
+    </div>
