87b68babc62a3125379facb2f2344933b84952a1
Vladislav <cnvvc_vlad@mail.ru>
2022-03-03 21:31:24 +0100

    updated files

--- vue/categories/index.php
+++ vue/categories/index.php
@@ -1,8 +1,6 @@
 <?php $title = 'Catégories des articles du blog ChessTeam'; ?>
 <?php $description =
     'Retrouvez toutes les catégories des articles publiés sur le site, regardez ses articles et ses commentaires'; ?>
-
-
 <h1>Voici toutes nos catégories dédiées aux jeux d'échecs</h1>
 <div class="top_article">
     <div class="view">
@@ -17,46 +15,7 @@
             <?php endforeach; ?>
         <?php endif; ?>
     </div>
-</div>
-
-
-    <h2>Créer une nouvelle catégorie</h2>
-
-    <form action="?action=categoryForm" method="post" class="form-create" enctype="multipart/form-data">
-        <fieldset>
-            <legend>Introduisez vos informations</legend>
-            <div class="form-create">
-                <label for="">Titre de la catégorie
-                    <p><input type="text" name="cat_title" value="" placeholder="Titre de la catégorie"></p>
-                </label>
-            </div>
-            <div class="form-create">
-                <label for="">Description de la catégorie
-                    <p><textarea name="cat_description" placeholder="Description de la catégorie" rows="5" cols="33"></textarea></p>
-                </label>
-            </div>
-            <div class="form-create">
-                <label class="form-create" for="image">Choisir une image
-                    <input type="file" class="image" id="image" name="image_category" accept='.gif, .png , .jpg' required>
-                </label>
-            </div>
-            <p><input type="hidden" name="id_user" value="<?php if (
-                isset($_SESSION['id_user'])
-            ) {
-                echo htmlspecialchars($_SESSION['id_user']);
-            } ?>"></p>
-
-            <div class="form-create">
-                <input type="submit" value="Envoyer" name="categoryCreation">
-
-                <p><input type="reset" value="Annuler" /></p>
-
-            </div>
-        </fieldset>
-
-    </form>
-
-
+</div> 
 <div class="back-page">
     <a href="<?php substr(basename($_SERVER['HTTP_REFERER']), 0); ?>">Retour</a>
     <a href="<?= dirname(SCRIPTS) ?>/">Retour à l'accueil</a>
