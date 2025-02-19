f58cca32242e991c4e7d87a4d776db12a0ef959c
Vladislav <cnvvc_vlad@mail.ru>
2025-02-19 10:15:24 +0100

    add status column & new update button

--- vue/admin/posts/index.php
+++ vue/admin/posts/index.php
@@ -14,6 +14,7 @@
                 <th>Titre</th>
                 <th>Publié le</th>
                 <th>Actions</th>
+                <th>Statut</th>
             </tr>
         </thead>
         <tbody>
@@ -35,8 +36,26 @@
                             SCRIPTS
                         ) ?>/admin/posts/delete/<?= $post->getId() ?>" 
                         method="post"
-                        onclick="return(confirm('Etes-vous sûr de vouloir supprimer?'));">
-                            <input type="submit" value="Supprimer">
+                        >
+                            <input onclick="return(confirm('Etes-vous sûr de vouloir supprimer?'));" type="submit" value="Supprimer">
+                        </form>
+                    </td>
+                    <td>                        
+                        <form 
+                        action="<?= dirname(
+                            SCRIPTS
+                        ) ?>/admin/posts/status/update ?>" 
+                        method="post"                        
+                        >
+                            <select name="status">
+                                <?php foreach ($params['status'] as $status): ?>
+                                    <option  value="<?= $status; ?>" <?= ($post->getArt_statut() === $status) ? 'selected' : ''; ?>>
+                                        <?= ucfirst($status); ?>                                    
+                                    </option>
+                                <?php endforeach; ?>
+                            </select>
+                            <input type="hidden" name="id" value="<?= $post->getId() ?>">                            
+                            <input onclick="return(confirm('Etes-vous sûr de vouloir modifier le statut?'));" type="submit" value="Modifier le statut">
                         </form>
                     </td>
                 </tr>
